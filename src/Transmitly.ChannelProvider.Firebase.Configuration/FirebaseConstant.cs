e20de0c268ed90803bd57dce59d12578fe20ece4
Jeremy <517429+JeremyMelton@users.noreply.github.com>
2025-07-27 19:05:47 -0600

    initial

---
+++ src/Transmitly.ChannelProvider.Firebase.Configuration/FirebaseConstant.cs
@@ -0,0 +1,27 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+namespace Transmitly.ChannelProvider.Firebase.Configuration
+{
+	/// <summary>
+	/// Common constants for firebase channel providers.
+	/// </summary>
+	public static class FirebaseConstant
+	{
+		/// <summary>
+		/// Default firebase identifier.
+		/// </summary>
+		public const string Id = "Firebase";
+	}
+}
